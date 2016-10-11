637239b1e8cd2e4e5727c4cf6c9217deb9ab4248
mda <mda@aistmail.com>
2016-10-11 15:34:35 +0600

    log to db

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -3,15 +3,24 @@ class LogController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    File.open(File.join("#{Rails.root}/lib", 'lib.rb'), 'a') do |f|
+    # File.open(File.join("#{Rails.root}/lib", 'lib.rb'), 'a') do |f|
+    #
+    # end
+    # @logs = File.read("#{Rails.root}/lib/lib.rb")
 
-    end
-    @logs = File.read("#{Rails.root}/lib/lib.rb")
+    @logs = Log.all
   end
 
   def create
-    File.open(File.join("#{Rails.root}/lib/", 'lib.rb'), 'a') do |f|
-      f.puts params[:logline]
+    # File.open(File.join("#{Rails.root}/lib/", 'lib.rb'), 'a') do |f|
+    log = Log.first
+    if log.nil?
+      log = Log.create
+      log.message = 'Logger'
+      log.save
     end
+    message_from_db = log.message + params[:logline]+"\n"
+    log.update(message: message_from_db)
+
   end
 end
