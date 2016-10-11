11bff4819e79647aaccf9e5771eb284594795c30
mda <mda@aistmail.com>
2016-10-11 10:30:40 +0600

    read and write to log file. Log file in lib folder

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -3,14 +3,14 @@ class LogController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    File.open(File.join(Rails.root, 'file.rb'), 'a') do |f|
+    File.open(File.join("#{Rails.root}/lib", 'lib.rb'), 'a') do |f|
 
     end
-    @logs = File.read("#{Rails.root}/file.rb")
+    @logs = File.read("#{Rails.root}/lib/lib.rb")
   end
 
   def create
-    File.open(File.join(Rails.root, 'file.rb'), 'a') do |f|
+    File.open(File.join("#{Rails.root}/lib/", 'lib.rb'), 'a') do |f|
       f.puts params[:logline]
     end
   end
